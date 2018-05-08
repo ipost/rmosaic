da2e0243d9d4f8160d83323e797c38afa2188223
Isaac Post <post.isaac@gmail.com>
2018-05-08 15:03:08 -0400

    generate color images in their own dir

--- png.rb
+++ png.rb
@@ -2,7 +2,7 @@ require 'chunky_png'
 X_DIM = 16
 Y_DIM = 16
 ARGV.map(&:upcase).each do |color|
-  filename = "images/#{color}.png"
+  filename = "color_images/#{color}.png"
   png = ChunkyPNG::Image.new(X_DIM, Y_DIM, color)
   png.save(filename)
   puts "wrote #{filename}"
